e609c364eda2b19e60cdc7804d09d78e585763a6
Alirea Hosseini <mehdimj0161@gmail.com>
2023-07-02 22:45:22 +0330

    Create password input

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,6 +1,8 @@
-import { User } from 'iconsax-react'
+import { User, Lock, Eye, EyeSlash } from 'iconsax-react'
+import { useState } from 'react'
 
 export default function LoginForm() {
+  const [isShowPassword, setIsShowPassword] = useState(false)
   return (
     <div id="login-form">
       <h2 className='text-center font-bold mb-10'>لطفا وارد شوید</h2>
@@ -8,29 +10,23 @@ export default function LoginForm() {
         <div className="input-group">
           <label htmlFor="">نام کاربری</label>
           <div className='input-parent'>
-            <User size={21} />
+            <User className='input-icon' size='21' />
             <input type="text" placeholder="نام کاربری خود را وارد کنید" />
           </div>
         </div>
         <div className="input-group">
-          <label htmlFor="">نام کاربری</label>
+          <label htmlFor="">کلمه عبور</label>
           <div className='input-parent'>
-            <User size={21} />
-            <input type="text" placeholder="نام کاربری خود را وارد کنید" />
-          </div>
-        </div>
-        <div className="input-group">
-          <label htmlFor="">نام کاربری</label>
-          <div className='input-parent'>
-            <User size={21} />
-            <input type="text" placeholder="نام کاربری خود را وارد کنید" />
-          </div>
-        </div>
-        <div className="input-group">
-          <label htmlFor="">نام کاربری</label>
-          <div className='input-parent'>
-            <User size={21} />
-            <input type="text" placeholder="نام کاربری خود را وارد کنید" />
+            <Lock className='input-icon' size="21" />
+            <input type={isShowPassword ? 'text' : 'password'} placeholder="نام کاربری خود را وارد کنید" />
+            <button type='button' className="show-pass-button" onClick={() => setIsShowPassword(prev => !prev)}>
+              {isShowPassword ? (
+                <EyeSlash size="21" />
+                ) : (
+                <Eye size="21" />
+              )}
+              
+            </button>
           </div>
         </div>
       </form>
